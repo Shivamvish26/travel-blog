945e858b415261746821cbaebb7e8f32df5276a9
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2024-02-27 16:37:12 +0530

    login page

--- src/Components/Pages/Login.js
+++ src/Components/Pages/Login.js
@@ -1,7 +1,12 @@
 import React from "react";
+import '../CSS/Login.css'
+import { Link } from "react-router-dom";
 
  export default function Login(){
-    return<div>
+    return<div className="login-cover">
         <h1>Login Page</h1>
+        <Link to="/Main">
+                <button>Go to Main Page</button>
+            </Link>
     </div>
 }
