b4b709b7c2ec40e941d46fb4c45bd657b22d944e
Shubham Vishwakarma <127945668+Shub26vish@users.noreply.github.com>
2024-02-28 16:08:31 +0530

    add pics and added code

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -3,11 +3,11 @@ import "./Navbar.css";
 import logo from "../Assets/logo-removebg-preview.png";
 import { Link } from "react-router-dom";
 import { Card, Row, Col, Input, DatePicker, Button } from "antd";
-import { SearchOutlined } from '@ant-design/icons';
-
+import { SearchOutlined } from "@ant-design/icons";
 
 const { Meta } = Card;
 const { RangePicker } = DatePicker;
+
 export default function Navbar() {
   const [selectedDates, setSelectedDates] = useState([]);
 
@@ -15,8 +15,13 @@ export default function Navbar() {
     setSelectedDates(dateStrings);
   };
 
-  const handleSearch = () => {
-    // Perform search using selected dates
+  const [place, setPlace] = useState("");
+  const [destination, setDestination] = useState("");
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    console.log("Select Place is", place);
+    console.log("Select Destination is", destination);
     console.log("Selected dates:", selectedDates);
   };
 
@@ -26,22 +31,26 @@ export default function Navbar() {
         <div className="logo">
           <img src={logo} alt="Logo" />
           <div className="logo-text">
-            <h1>Your Text Here</h1>
-            <h2>The World</h2>
+            <h1>Fall down seven times and </h1>
+            <h2>Stand up eight.</h2>
           </div>
           <div className="card-container">
             <Card className="centered-card">
               <Row gutter={[16, 16]}>
-                <Col span={6}>
+                <Col span={8}>
                   <Input
                     className="inputfields"
-                    placeholder="Enter something"
+                    placeholder="Enter Place"
+                    value={place}
+                    onChange={(e) => setPlace(e.target.value)}
                   />
                 </Col>
-                <Col span={6}>
+                <Col span={8}>
                   <Input
                     className="inputfields"
-                    placeholder="Enter something"
+                    placeholder="Enter Destination"
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
                   />
                 </Col>
                 <Col span={6}>
@@ -50,13 +59,14 @@ export default function Navbar() {
                     onChange={handleDateChange}
                   />
                 </Col>
-                <Col span={6}>
-                <Button
+                <Col span={2}>
+                  <Button
                     type="primary"
                     className="inputfields"
                     onClick={handleSearch}
-                    icon={<SearchOutlined />}
-                  />
+                  >
+                    Search
+                  </Button>
                 </Col>
               </Row>
             </Card>
--- src/Components/Pages/Main.js
+++ src/Components/Pages/Main.js
@@ -1,9 +1,55 @@
 import React from "react";
-import '../CSS/Main.css'
-function Main(){
-    return<div>
-        <h1>Main page</h1>
+import "../CSS/Main.css";
+import { EnvironmentOutlined } from "@ant-design/icons";
+import { Row, Card, Col } from "antd";
+import ganpatipule from '../Assets/Ganpatipule.jpg'
+import Alibag from '../Assets/ALIBAG3.jpg'
+import Harihareshwar from '../Assets/Harihareshwar-Beach-1024x601.webp'
+function Main() {
+  return (
+    <div className="container">
+      <div className="row">
+        <h3 className="textColor">
+          FIND <span style={{ color: "#6b4642" }}>POPULAR</span>
+          <EnvironmentOutlined className="location-icon" />
+        </h3>
+        <h4 className="textColor">DESITINATIONS</h4>
+      </div>
+
+      <Row gutter={[16, 16]}>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{ width: "100%" }}
+            cover={<img alt="example" src={ganpatipule} />}
+          >
+            <h3>Ganpatipule</h3>
+            <h6>Konkan, Maharashtra </h6>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{ width: "100%" }}
+            cover={<img alt="example" src={Alibag} style={{height: '292px'}}/>}
+          >
+            <h3>Alibaug</h3>
+            <h6>Mumbai, Maharashtra </h6>
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card
+            hoverable
+            style={{ width: "100%" }}
+            cover={<img alt="example" src={Harihareshwar} style={{height: '292px'}}/>}
+          >
+             <h3>Harihareshwar</h3>
+            <h6>Raigad, Maharashtra </h6>
+          </Card>
+        </Col>
+      </Row>
     </div>
+  );
 }
 
-export default Main
+export default Main;
